ac56bfbb669991854f2ce03a33e5fd5aba6c1a52
Wouter Castelein <wmcastelein@gmail.com>
2017-12-18 15:44:44 +0100

    We kunnen eindelijk bats gebruiken

--- NetwProg/NetwProg/Program.cs
+++ NetwProg/NetwProg/Program.cs
@@ -14,22 +14,35 @@ namespace NetwProg
         static Server server;
         static void Main(string[] args)
         {
-            ReadInput();
-            server = new Server(port);
+            ReadInput(args);
+
             ioThread = new Thread(() => CreateInputThread());
             ioThread.Start();
         }
 
-        static void ReadInput()
+        static void ReadInput(string[] args)
         {
-            string[] input = Console.ReadLine().Split();
+            string[] input = args;
             Console.Title = "NetChange " + input[0];
             port = int.Parse(input[0]);
             Data.AddNDisEntry(port);
             //Data.ndis[port].AddPath
+            server = new Server(port);
             for (int i = 1; i < input.Length; i++)
             {
                 int nbport = int.Parse(input[i]);
+                while (!Data.connections.ContainsKey(nbport))
+                {
+                    try
+                    {
+                        Connection newConnection = new Connection(nbport);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Trying to connect to " + nbport);
+                        Thread.Sleep(50);
+                    }
+                }
                 //Connection newConnection = new Connection(nbport);
                 Data.AddNDisEntry(nbport);
                 Data.ndis[nbport].AddPath(nbport, 0);
